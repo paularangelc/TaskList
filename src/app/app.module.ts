92c00b5045c809a12b244d208b557ea3f0b06ff1
paularangelc <paularangelc@gmail.com>
2022-07-20 12:53:23 -0300

    Projeto Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes:Routes = [
+  {path:'', component: TasksComponent},
+  {path:'about', component: AboutComponent}
+]
+
 
 
 @NgModule({
@@ -16,12 +27,17 @@ import { TasksItemComponent } from './components/tasks-item/tasks-item.component
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TasksItemComponent
-  ],
+    TasksItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
+   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
